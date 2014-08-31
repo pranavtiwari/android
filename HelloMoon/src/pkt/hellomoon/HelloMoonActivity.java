cc476e4b33490e21ed314c4f489b8667ae3601fd
pranavtiwari@gmail.com <pranavtiwari@gmail.com>
2014-08-31 22:55:50 +0530

    .

---
+++ HelloMoon/src/pkt/hellomoon/HelloMoonActivity.java
@@ -0,0 +1,15 @@
+package pkt.hellomoon;
+
+import com.bignerdranch.android.hellomoon.R;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class HelloMoonActivity extends FragmentActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_moon);
+    }
+}
