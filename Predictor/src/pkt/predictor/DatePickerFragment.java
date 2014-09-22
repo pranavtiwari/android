b43cb55aa8f7c5895ed97900a1ae12c6b290c88b
pranavtiwari@gmail.com <pranavtiwari@gmail.com>
2014-09-22 22:25:26 +0530

    checkpoint: book 225 - modify trigger date

--- Predictor/src/pkt/predictor/DatePickerFragment.java
+++ Predictor/src/pkt/predictor/DatePickerFragment.java
@@ -1,20 +1,83 @@
 package pkt.predictor;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.DatePicker.OnDateChangedListener;
 
 public class DatePickerFragment extends DialogFragment {
+	public static final String EXTRA_DATE = "pkt.predictor.date";
+	
+	private Date mDate;
+
+	public static DatePickerFragment newInstance(Date date) {
+		Bundle args = new Bundle();
+		args.putSerializable(EXTRA_DATE, date);
+		
+		DatePickerFragment fragment = new DatePickerFragment();
+		fragment.setArguments(args);
+		
+		return fragment;
+	}
+	
 	@Override
 	public Dialog onCreateDialog(Bundle savedState) {
+		mDate = (Date)getArguments().getSerializable(EXTRA_DATE);
+		
+		// Create a calendar to get date
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(mDate);
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		
 		View v = getActivity().getLayoutInflater()
 				.inflate(R.layout.dialog_date, null);
+		
+		DatePicker datePicker = (DatePicker)v.findViewById(R.id.dialog_date_datePicker);
+		OnDateChangedListener onDateChangedListener = new OnDateChangedListener() {
+			public void onDateChanged(DatePicker view, int year, int month, int day) {
+				mDate = new GregorianCalendar(year, month, day).getTime();
+				getArguments().putSerializable(EXTRA_DATE, mDate);
+			}
+		};
+
+		datePicker.init(year, month, day, onDateChangedListener);
+
 		return new AlertDialog.Builder(getActivity())
 			.setView(v)
 			.setTitle(R.string.trigger_date)
-			.setPositiveButton(android.R.string.ok, null)
+			.setPositiveButton(android.R.string.ok,
+					new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							sendResult(Activity.RESULT_OK);
+						}
+					})
 			.create();
+	}
+	
+	private void sendResult(int resultCode) {
+		if (getTargetFragment() == null) {
+			return;
+		}
+		
+		Intent i = new Intent();
+		i.putExtra(EXTRA_DATE, mDate);
+		
+		getTargetFragment()
+			.onActivityResult(getTargetRequestCode(), resultCode, i);
 	}
 }
--- Predictor/src/pkt/predictor/PredictionFragment.java
+++ Predictor/src/pkt/predictor/PredictionFragment.java
@@ -1,7 +1,10 @@
 package pkt.predictor;
 
+import java.util.Date;
 import java.util.UUID;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -24,6 +27,7 @@ public class PredictionFragment extends Fragment {
 	
 	private final String TAG = "PredictorFragment";
 	private static final String DIALOG_DATE = "date";
+	private static final int REQUEST_DATE = 0;
 	
 	public static final String EXTRA_PREDICTION_ID = "pkt.predictor.prediction_id";
 	
@@ -55,13 +59,15 @@ public class PredictionFragment extends Fragment {
 		});
 		
 		mTriggerDateButton = (Button)v.findViewById(R.id.trigger_date);
-		mTriggerDateButton.setText(mPrediction.getTriggerDate().toString());
+		updateTriggerDate();
 		mTriggerDateButton.setEnabled(true);
 		mTriggerDateButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				FragmentManager fm = getActivity()
 					.getSupportFragmentManager();
-				DatePickerFragment dialog = new DatePickerFragment();
+				DatePickerFragment dialog = DatePickerFragment
+						.newInstance(mPrediction.getTriggerDate());
+				dialog.setTargetFragment(PredictionFragment.this, REQUEST_DATE);
 				dialog.show(fm, DIALOG_DATE);
 			}
 		});
@@ -73,7 +79,19 @@ public class PredictionFragment extends Fragment {
 		return v;
 	}
 	
+	@Override
+	public void onActivityResult(int req, int result, Intent data) {
+		if (result != Activity.RESULT_OK) return;
+		if (req == REQUEST_DATE) {
+			Date date = (Date)data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+			mPrediction.setTriggerDate(date);
+			updateTriggerDate();
+		}
+	}
 
+	private void updateTriggerDate() {
+		mTriggerDateButton.setText(mPrediction.getTriggerDate().toString());
+	}
 	public static PredictionFragment newInstance(UUID id) {
 		Bundle args = new Bundle();
 		args.putSerializable(EXTRA_PREDICTION_ID, id);
